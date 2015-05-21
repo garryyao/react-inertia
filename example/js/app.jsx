77e2b53c83d3290800f97a7bd9420afe8f31dc9a
Garry Yao <yaojun85@gmail.com>
2015-05-21 18:04:41 +0800

    Update 2015-05-21T10:04:37.932Z

--- example/js/app.jsx
+++ example/js/app.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
+require.ensure(['../../scrollable'], function () {
+  var React = require('react');
+  var Scrollable = require('../../scrollable');
 
-import Scrollable from '../../dist/scrollable.js';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    var itemElements = [];
-
-    var color;
-    for (var i = 0; i < this.props.itemsCount; i++) {
-      color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-      itemElements.push(<div className="item" style={{'background-color':color}}>item {i}</div>);
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
     }
 
-    return (
-      <div>
-        <Scrollable className="area" contentClassName="content">
-          {itemElements}
-        </Scrollable>
-      </div>
-    );
+    render() {
+      var itemElements = [];
+
+      var color;
+      for (var i = 0; i < this.props.itemsCount; i++) {
+        color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        itemElements.push(<div className="item" style={{'background-color':color}}>item {i}</div>);
+      }
+
+      return (
+        <div>
+          <Scrollable className="area" contentClassName="content">
+            {itemElements}
+          </Scrollable>
+        </div>
+      );
+    }
   }
-}
 
-App.defaultProps = {
-  itemsCount: 100
-};
+  App.defaultProps = {
+    itemsCount: 100
+  };
 
-export default App;
+  module.exports = App;
+});
+
--- example/js/main.js
+++ example/js/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from './app.jsx';
 
-var css = require("style!css!../../dist/css/scrollbar.css");
+var css = require("style!css!../../css/scrollbar.css");
 
 React.render(React.createElement(App, {itemsCount: 100}), document.getElementById("main"));
